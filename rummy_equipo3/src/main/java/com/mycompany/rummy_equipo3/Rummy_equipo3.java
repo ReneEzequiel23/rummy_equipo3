5de03e21ca440dccbc6156b2809cac588c36e7a1
ReneEzequiel23 <150867485+ReneEzequiel23@users.noreply.github.com>
2024-09-24 12:46:35 -0700

    rummy

---
+++ rummy_equipo3/src/main/java/com/mycompany/rummy_equipo3/Rummy_equipo3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rummy_equipo3;
+
+/**
+ *
+ * @author filor
+ */
+public class Rummy_equipo3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
